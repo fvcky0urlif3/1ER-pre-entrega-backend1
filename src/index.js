ad274691dfb3e05ff0a6301ad1d75010469564b3
Fvcky0urlif3 <facuquaglia@hotmail.com>
2024-10-23 13:18:07 -0300

    router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from "express";
-import ProductManager from "./controllers/ProductManager.js ";
+import ProductRouter from "./Router/product.routes.js";
 
-const product = new ProductManager();
 
 const app = express();
 const PORT = 4020;
@@ -9,31 +8,7 @@ const PORT = 4020;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/products", async (req, res) => {
-    res.send(await product.getProducts())
-}) 
-app.get("/products/:id", async (req, res) => {
-    let id = req.params.id
-    res.send(await product.getProductsById(id))
-}) 
-
-app.post("/products", async (req, res) => {
-   let newProduct = req.body; 
-   res.send(await product.addProducts(newProduct));
-})
-
-app.put("/products/:id", async (req, res) => {
-    let id = req.params.id
-    let updateProduct = req.body;
-    res.send(await product.updateProducts(id, updateProduct))
-})
-
-app.delete("/products/:id", async (req, res) => {
-    let id = req.params.id
-    res.send(await product.deleteProducts(id))
-} )
-
-
+app.use("/products", ProductRouter)
 
 
 app.listen(PORT, () => {
