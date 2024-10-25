43a7de4e2b6cebab64976f118a4c6351637dcc33
Fvcky0urlif3 <facuquaglia@hotmail.com>
2024-10-25 00:40:24 -0300

    cart encaminado

--- src/Router/carts.routes.js
+++ src/Router/carts.routes.js
@@ -1,19 +1,29 @@
-import { Router } from "express";
+import { Router } from "express"; 
 import CartManager from "../controllers/CartManager.js";
 
-const CartRouter = Router()
-const carts = new CartManager 
+const CartRouter = Router();
+const carts = new CartManager();
 
+// Ruta para agregar un carrito
 CartRouter.post("/", async (req, res) => {
     res.send(await carts.addCarts());
 });
 
-CartRouter.get('/:id', async (req, res)=> {
+// Ruta para obtener todos los carritos
+CartRouter.get('/', async (req, res) => {
     res.send(await carts.readCarts());
 });
 
-CartRouter.get('/', async (req, res)=> {
+// Ruta para obtener un carrito específico por ID
+CartRouter.get('/:id', async (req, res) => {
     res.send(await carts.getCartsById(req.params.id));
 }); 
 
-export default CartRouter  
+// Ruta para agregar un producto a un carrito específico
+CartRouter.post('/:cid/products/:pid', async (req, res) => {
+    let cartId = req.params.cid;
+    let productId = req.params.pid;
+    res.send(await carts.addProductInCart(cartId, productId));
+});
+
+export default CartRouter;
--- src/controllers/CartManager.js
+++ src/controllers/CartManager.js
@@ -1,5 +1,8 @@
 import { promises as fs } from 'fs';
 import { nanoid } from 'nanoid';
+import ProductManager from './ProductManager.js';
+
+const productALL = new ProductManager   
 
 class CartManager {
     constructor() {
@@ -34,6 +37,30 @@ class CartManager {
         if (!cartById) return "Carrito inexistente";
         return cartById;
     };
+
+    addProductInCart = async (cartId, productId) => {
+        let cartById = await this.exist(cartId);
+        if (!cartById) return "Carrito inexistente";
+        let productById = await productALL.exist(productId);
+        if (!productById) return "Producto inexistente";
+    
+        let cartsALL = await this.readCarts();
+        let cartFilter = cartsALL.filter(cart => cart.id != cartId);
+    
+        if (cartById.products.some(prod => prod.id === productId)) {
+            let productInCart = cartById.products.find(prod => prod.id === productId);
+            productInCart.cantidad++;
+            let cartsConcat = [{ ...cartById }, ...cartFilter]; // Se mantiene el carrito actualizado
+            await this.writeCarts(cartsConcat);
+            return "Producto sumado exitosamente";
+        }
+    
+        cartById.products.push({ id: productById.id, cantidad: 1 });
+        let cartsConcat = [{ ...cartById }, ...cartFilter];
+        await this.writeCarts(cartsConcat);
+        return "Producto agregado exitosamente";
+    };
+
 }
 
 export default CartManager
